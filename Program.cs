2cdfb81d1db1c8c7b2b6b9029352c135e42804d1
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-06-11 16:34:56 +0500

    Add Project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+string gameInput = Console.ReadLine();
+Console.Write("Your age :");
+int age = Convert.ToInt32(gameInput);
+bool isCanPlay = age > 12 && age <40;
+
+if(isCanPlay == true)
+{
+    Console.WriteLine("You have to try play this game");
+}
