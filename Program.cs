faae10075755be11bce1fed020e3e645f3f13d02
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-06-13 09:41:02 +0500

    Vazifa

--- Program.cs
+++ Program.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿//string userInput = Console.ReadLine();
+//int age = Convert.ToInt32(userInput);
+//bool isArmyAge = age is > 17 and < 27;
+//string message = isArmyAge
+    //? "You should go to army!"
+   // : "You cannot go to army!";
+// Console.WriteLine(message);
 
-string gameInput = Console.ReadLine();
-Console.Write("Your age :");
-int age = Convert.ToInt32(gameInput);
-bool isCanPlay = age > 12 && age <40;
+//string username = Console.ReadLine();
+//switch(username)
+//{
+  //  case "Elbek":
+    //    Console.WriteLine("Teacher is here");
+      //  break;
+    //case "Izzatillo":
+      //  Console.WriteLine("I am here");
+        //break;
+    //default:    
+     //   Console.WriteLine("Noone is here");
+       // break;     
+//}
 
-if(isCanPlay == true)
+//string usersname = Console.ReadLine();
+//if (usersname == "Elbek")
+//{
+  //  Console.WriteLine("Teacher is here");
+//}
+//else if (usersname == "Izzatillo")
+//{
+ //   Console.WriteLine("I am here");
+//}
+//else
+//{
+ //   Console.WriteLine("None is here");
+//}
+
+using System;
+// if -> tenary operator
+// switch -> pattern matching
+string username = Console.ReadLine();
+string message = username switch
 {
-    Console.WriteLine("You have to try play this game");
-}
+    "Elbek" => "Teacher is here",
+    "Izzatillo" => "I am here",
+    _ => "Noone is here"
+};
+Console.WriteLine(message);
